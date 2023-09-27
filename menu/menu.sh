050038a6decea1ca56a19683f344b9a54fd26dec
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-27 17:45:29 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,78 +1,19 @@
-#!/bin/bash 
- # ========================================= 
- vlx=$(grep -c -E "^#& " "/etc/xray/config.json") 
- let vla=$vlx/2 
- vmc=$(grep -c -E "^### " "/etc/xray/config.json") 
- let vma=$vmc/2 
- ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)" 
-  
- trx=$(grep -c -E "^#! " "/etc/xray/config.json") 
- let tra=$trx/2 
- ssx=$(grep -c -E "^## " "/etc/xray/config.json") 
- let ssa=$ssx/2 
- COLOR1='\033[0;35m' 
- COLOR2='\033[0;39m' 
- clear
-
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ "$res" = "Expired" ]; then
-Exp="\e[36mExpired\033[0m"
-else
-Exp=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini | grep $MYIP | awk '{print $3}')
-fi
-
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
+#!/bin/bash
+clear
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+## Foreground
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
 BIGreen='\033[1;92m'      # Green
 BIYellow='\033[1;93m'     # Yellow
 BIBlue='\033[1;94m'       # Blue
@@ -91,26 +32,6 @@ IPurple='\033[0;95m'      # Purple
 ICyan='\033[0;96m'        # Cyan
 IWhite='\033[0;97m'       # White
 NC='\e[0m'
-#Download/Upload today
-dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
-utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
-ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload yesterday
-dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
-uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
-tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload current month
-dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
-umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
-tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
-clear
-
-# // Exporting Language to UTF-8
-
-export LANG='en_US.UTF-8'
-export LANGUAGE='en_US.UTF-8'
-
-
 # // Export Color & Information
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
@@ -120,35 +41,7 @@ export PURPLE='\033[0;35m'
 export CYAN='\033[0;36m'
 export LIGHT='\033[0;37m'
 export NC='\033[0m'
-
-###########- END COLOR CODE -##########
-tram=$( free -h | awk 'NR==2 {print $2}' )
-uram=$( free -h | awk 'NR==2 {print $3}' )
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-
-# // Export Banner Status Information
-export EROR="[${RED} EROR ${NC}]"
-export INFO="[${YELLOW} INFO ${NC}]"
-export OKEY="[${GREEN} OKEY ${NC}]"
-export PENDING="[${YELLOW} PENDING ${NC}]"
-export SEND="[${YELLOW} SEND ${NC}]"
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
-
-# // Export Align
-export BOLD="\e[1m"
-export WARNING="${RED}\e[5m"
-export UNDERLINE="\e[4m"
-
-# TOTAL RAM
-total_ram=` grep "MemTotal: " /proc/meminfo | awk '{ print $2}'`
-totalram=$(($total_ram/1024))
-
-persenmemori="$(echo "scale=2; $usmem*100/$tomem" | bc)"
-#persencpu=
-persencpu="$(echo "scale=2; $cpu1+$cpu2" | bc)"
-
-# // Clear
+# // STATUS VPN
 clear
 clear && clear && clear
 clear;clear;clear
@@ -194,135 +87,183 @@ resv2r="${green}ON${NC}"
 else
 resv2r="${red}OFF${NC}"
 fi
-function addhost(){
-clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo ""
-read -rp "Domain/Host: " -e host
-echo ""
-if [ -z $host ]; then
-echo "????"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -n 1 -s -r -p "Press any key to back on menu"
-setting-menu
+
+BURIQ () {
+MYIP=$(curl -sS ipv4.icanhazip.com)
+curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini > /root/tmp
+data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+for user in "${data[@]}"
+do
+exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+d1=(`date -d "$exp" +%s`)
+d2=(`date -d "$biji" +%s`)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+echo $user > /etc/.$user.ini
 else
-echo "IP=$host" > /var/lib/SIJA/ipvps.conf
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "Dont forget to renew cert"
-echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+rm -f /etc/.$user.ini > /dev/null 2>&1
+fi
+done
+rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
 fi
 }
-function genssl(){
-clear
-systemctl stop nginx
-domain=$(cat /var/lib/SIJA/ipvps.conf | cut -d'=' -f2)
-Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
-if [[ ! -z "$Cek" ]]; then
-sleep 1
-echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by $Cek " 
-systemctl stop $Cek
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Processing to stop $Cek " 
-sleep 1
-fi
-echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
-sleep 2
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
-echo -e "[ ${green}INFO${NC} ] Renew cert done... " 
-sleep 2
-echo -e "[ ${green}INFO${NC} ] Starting service $Cek " 
-sleep 2
-echo $domain > /etc/xray/domain
-systemctl restart xray
-systemctl restart nginx
-echo -e "[ ${green}INFO${NC} ] All finished... " 
-sleep 0.5
-echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
 }
-export sem=$( curl -s https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/versions)
-export pak=$( cat /home/.ver)
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/permission/ipmini | grep $MYIP | awk '{print $3}')
+fi
+#Domain & IPVPS
+domain=$(cat /root/domain)
 IPVPS=$(curl -s ipinfo.io/ip )
-clear
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-
-echo -e "${BIYellow}□ Server Uptime       = ${GREEN}$( uptime -p  | cut -d " " -f 2-10000 ) ${NC}"
-echo -e "${BIYellow}□ Current Time        = ${GREEN}$( date -d "0 days" +"%d-%m-%Y | %X" )${NC}"
-echo -e "${BIYellow}□ Operating System    = ${GREEN}$( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' ) ( $( uname -m) )${NC}"
-echo -e "${BIYellow}□ Current Domain      = ${GREEN}$( cat /etc/xray/domain )${NC}"
-echo -e "${BIYellow}□ NS Domain           = ${GREEN}$(cat /root/nsdomain)${NC}"
-echo -e "${BIYellow}□ Jumlah Ram          = ${GREEN}${totalram}MB"
-echo -e "${BIYellow}□ Server Resource     = $uram / $tram"
-echo -e "${BIYellow}□ Clients Name        = ${GREEN}$Name${NC}"
-echo -e "${BIYellow}□ Expired Script VPS  = ${GREEN}$Exp${NC}"
-echo -e "${BIYellow}□ Time Reboot VPS     = 00:00 ${GREEN}( Jam 12 Malam )${NC}"
-echo -e "${BIYellow}□ License Limit       = ORDER ${GREEN}( 082131861788 )${NC}"
-echo -e "${BIYellow}□ AutoScript By Dev   = Wak ${GREEN}( todfix667 )${NC}"
-
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "${BICyan} │         ${BIWhite}${UWhite}todfix667 TUNNELING${NC}     ${BICyan}│${NC}"
-echo -e "${BICyan} │                                                     │${NC}"
-echo -e " ${BICyan}│  ${BICyan}Use Core        :  ${BIPurple}XRAY${NC}      ${BICyan}│${NC}"
-echo -e " ${BICyan}│  ${BICyan}IP-VPS          :  ${BIYellow}$IPVPS   ${BICyan}│${NC}"
-echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
-
-echo -e "${GREEN}Just a little patience${NC}" 
-
-echo -e "${GREEN}┌──────────────────────────────────────────────────┐${NC}" 
-echo -e "${GREEN}│  \033[0m ${BOLD}${YELLOW}SSH     VMESS       VLESS      TROJAN $NC" 
-echo -e "${GREEN}│  \033[0m ${Blue} $ssh1        $vma           $vla          $tra $NC" 
-echo -e "${GREEN}└──────────────────────────────────────────────────┘${NC}" 
+ISPVPS=$( curl -s ipinfo.io/org )
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
+cpu_usage+="%"
+cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
+cores=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
+freq=$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo)
+# OS Uptime
+uptime="$(uptime -p | cut -d " " -f 2-10)"
+# RAM Info
+tram=$(free -m | awk 'NR==2 {print $2}')
+uram=$(free -m | awk 'NR==2 {print $3}')
+# Total BANDWIDTH
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
+#total user account
+vlx=$(grep -c -E "^#& " "/etc/xray/config.json") 
+ let vla=$vlx/2 
+ vmc=$(grep -c -E "^### " "/etc/xray/config.json") 
+ let vma=$vmc/2 
+ ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)" 
   
+ trx=$(grep -c -E "^#! " "/etc/xray/config.json") 
+ let tra=$trx/2 
+ ssx=$(grep -c -E "^## " "/etc/xray/config.json") 
+ let ssa=$ssx/2 
+ COLOR1='\033[0;35m' 
+ COLOR2='\033[0;39m' 
+ clear
+echo ""
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "     ${WB}SSH XRAYS Websocket Autoscript By NevermoreSSH${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}»»» Server Information «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}OS      :  "$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-) ${NC}         
+echo -e "  ${RB}♦️${NC} ${YB}KERNEL  :  $(uname -r) ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}UPTIME  :  $uptime ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}CPU     :  ${cores}cores @${freq}MHz (${cpu_usage}) ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}RAM     :  ${uram}MB / ${tram}MB ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}DOMAIN  :  $domain ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}NS-HOST :  $(cat /root/nsdomain) ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}IP      :  $IPVPS ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}IPSVPS  :  $ISPVPS ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}CLIENT  :  $Name ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}EXP     :  $exp ($exp2 days) ${NC} "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                 ${WB}»»» SSH/XRAYS Menu «««${NC}          "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}01.${NC} ${YB}SSH WEBSOCKET   $WB[${ssh1} User] ${NC}"
+echo -e "  ${RB}02.${NC} ${YB}XRAY VMESS WS   $WB[${vma} User] ${NC}"
+echo -e "  ${RB}03.${NC} ${YB}XRAY VLESS WS   $WB[${vla} User] ${NC}"
+echo -e "  ${RB}04.${NC} ${YB}XRAY TROJAN WS  $WB[${tra} User] ${NC}"
+echo -e "  ${RB}05.${NC} ${YB}XRAY TRIAL WS ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "     ${BICyan} SSH ${NC}: $ressh"" ${BICyan} NGINX ${NC}: $resngx"" ${BICyan}  XRAY ${NC}: $resv2r"" ${BICyan} TROJAN ${NC}: $resv2r"
 echo -e "   ${BICyan}     STUNNEL ${NC}: $resst" "${BICyan} DROPBEAR ${NC}: $resdbr" "${BICyan} SSH-WS ${NC}: $ressshws"
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "     ${BICyan}[${BIWhite}01${BICyan}] SSH     ${BICyan}[${BIYellow}Menu${BICyan}]${NC}"    "     ${BICyan}[${BIWhite}06${BICyan}] TRIALL    ${BICyan}[${BIYellow}Menu${BICyan}]${NC}" "${BICyan}   │"
-echo -e "     ${BICyan}[${BIWhite}02${BICyan}] VMESS   ${BICyan}[${BIYellow}Menu${BICyan}]${NC}" "     ${BICyan}[${BIWhite}07${BICyan}] BACKUP    ${BICyan}[${BIYellow}Menu${BICyan}]${NC}" "${BICyan}   │"
-echo -e "     ${BICyan}[${BIWhite}03${BICyan}] VLESS   ${BICyan}[${BIYellow}Menu${BICyan}]${NC}"  "     ${BICyan}[${BIWhite}08${BICyan}] ADD-HOST      ${NC}" "${BICyan}    │"
-echo -e "     ${BICyan}[${BIWhite}04${BICyan}] TROJAN  ${BICyan}[${BIYellow}Menu${BICyan}]${NC}" "     ${BICyan}[${BIWhite}09${BICyan}] RUNNING              │"
-echo -e "     ${BICyan}[${BIWhite}05${BICyan}] SETING  ${BICyan}[${BIYellow}Menu${BICyan}]${NC}"  "     ${BICyan}[${BIWhite}10${BICyan}] SET REBOOT${BICyan}[${BIYellow}Menu${BICyan}]${NC}" "${BICyan}   │${NC}"
-echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
-echo -e "${BICyan}                  MENU TAMBAHAN${NC} "
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "     ${BICyan}[${BIWhite}11${BICyan}] DOMAIN FREE${NC} "
-echo -e "     ${BICyan}[${BIWhite}12${BICyan}] UPDATE MENU${NC} "
-echo -e "     ${BICyan}[${BIWhite}13${BICyan}] GANTI NS DOMAIN${NC} "
-echo -e "     ${BICyan}[${BIWhite}99${BICyan}] CLEAR SAMPAH${NC} "
-echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "${BICyan} │$NC ${BICyan}HARI ini${NC}: ${red}$ttoday$NC ${BICyan}KEMARIN${NC}: ${red}$tyest$NC ${BICyan}BULAN${NC}: ${red}$tmon$NC $NC"
-echo -e "${BICyan} └─────────────────────────────────────────────────────┘${NC}"
-echo -e " ${BICyan}┌─────────────────────────────────────┐${NC}"
-echo -e " ${BICyan}│  Version      ${NC} : v4. Last Update"
-echo -e " ${BICyan}│  User       ${NC}   :\033[1;36m $Name \e[0m"
-echo -e " ${BICyan}│  Day Expired${NC}   : $exp2  DAY"
-echo -e " ${BICyan}└─────────────────────────────────────┘${NC}"
-echo
-read -p " Select menu : " opt
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                   ${WB}»»» VPS Menu «««${NC}                 "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}06.${NC} ${YB}SETTING VPS ${NC}          ${RB}55.${NC} ${YB}INSTALL TCP BBR ${NC}"
+echo -e "  ${RB}07.${NC} ${YB}BACKUP/RESTORE ${NC}       ${RB}66.${NC} ${YB}INSTALL BBR+ 5.15 ${NC}"
+echo -e "  ${RB}08.${NC} ${YB}SET REBOOT ${NC}           ${RB}77.${NC} ${YB}XRAYCORE MOD 1.6.5-1 ${NC}"       
+echo -e "  ${RB}09.${NC} ${YB}ADD DOMAIN/HOST ${NC}      ${RB}88.${NC} ${YB}XRAYCORE MOD 1.7.2-1 ${NC}"      
+echo -e "  ${RB}10.${NC} ${YB}ADD NS-DOMAIN ${NC}        ${RB}99.${NC} ${YB}XRAYCORE LATEST 1.8.4 ${NC}"
+echo -e "  ${RB}11.${NC} ${YB}RENEW CERT XRAYS ${NC}"
 echo -e ""
-case $opt in
+echo -e "  ${RB}12.${NC} ${YB}CHECK VPN PORT ${NC}"
+echo -e "  ${RB}13.${NC} ${YB}CHECK VPN STATUS ${NC}"
+echo -e "  ${RB}14.${NC} ${YB}CHECK CPU & RAM ${NC}"
+echo -e "  ${RB}15.${NC} ${YB}CHECK BANDWIDTH ${NC}"
+echo -e "  ${RB}16.${NC} ${YB}DNS CHANGER ${NC}"
+echo -e "  ${RB}17.${NC} ${YB}NETFLIX CHECKER ${NC}"
+echo -e "  ${RB}18.${NC} ${YB}CLEAR CACHE/RAM ${NC}"
+echo -e "  ${RB}19.${NC} ${YB}CLEAR EXP USERS ${NC}"
+echo -e "  ${RB}20.${NC} ${YB}SPEEDTEST VPS ${NC}"
+echo -e "  ${RB}21.${NC} ${YB}RESTART VPN SERVICE ${NC}"
+echo -e "  ${RB}22.${NC} ${YB}REBOOT ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                 ${WB}»»» Total Bandwidth «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Daily Data Usage    : $ttoday ${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Monthly Data Usage  : $tmon ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo ""
+read -p "Select From Options [ 1 - 99 ] : " menu
+case $menu in
 1) clear ; menu-ssh ;;
 2) clear ; menu-vmess ;;
 3) clear ; menu-vless ;;
 4) clear ; menu-trojan ;;
-5) clear ; menu-set ;;
-6) clear ; menu-trial ;;
+5) clear ; menu-trial ;;
+6) clear ; menu-set ;;
 7) clear ; menu-backup ;;
-8) clear ; addhost ;;
-9) clear ; running ;;
-10) clear ; jam ;;
-11) clear ; cf ;;
-12) clear ; up ;;
-13) clear ; slow ;;
-99) clear ; clearcache ;;
-6969) clear ; wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
+8) clear ; jam ;;
+9) clear ; add-host ;;
+10) clear ; slow ;;
+11) clear ; genssl ;;
+12) clear ; cat log-install.txt ;;
+13) clear ; running ;;
+14) clear ; htop ;;
+15) clear ; vnstat ;;
+16) clear ; dns1 ;;
+17) clear ; netf ;;
+18) clear ; clearcache ;;
+19) clear ; delete && xp ;;
+20) clear ; speedtest ;;
+21) clear ; restart ;;
+22) clear ; reboot ;;
+55) clear ; bbr3 ;;
+66) clear ; bbr4 ;;
+77) clear ; mv /usr/local/bin/xray /usr/local/bin/xray.bakk && wget -q -O /usr/local/bin/xray "https://github.com/NevermoreSSH/Xcore-custompath/releases/download/Xray-linux-64-v1.6.5.1/Xray-linux-64-v1.6.5.1" && chmod 755 /usr/local/bin/xray && xray version ;;
+88) clear ; mv /usr/local/bin/xray /usr/local/bin/xray.bakk && wget -q -O /usr/local/bin/xray "https://github.com/NevermoreSSH/Xcore-custompath/releases/download/Xray-linux-64-v1.7.2-1/Xray-linux-64-v1.7.2-1" && chmod 755 /usr/local/bin/xray && xray version ;;
+99) clear ; mv /usr/local/bin/xray /usr/local/bin/xray.bakk && wget -q -O /usr/local/bin/xray "https://github.com/NevermoreSSH/Xcore-custompath/releases/download/Xray-linux-64-v1.8.4/Xray-linux-64-v1.8.4" && chmod 755 /usr/local/bin/xray && xray version ;;
+9999) clear ; wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo -e "" ; echo "Press any key to back exit" ; sleep 1 ; exit ;;
