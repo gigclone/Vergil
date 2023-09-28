90a7f55e220608a153d47879f033b787db96bf05
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-28 11:04:21 +0800

    Update slowdns.sh

--- autoscript-ssh-slowdns/slowdns.sh
+++ autoscript-ssh-slowdns/slowdns.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
-# Script  By CRSe7en2nd
+# Script  By NevermoreSSH
 # 2023 SLOWDNS
 # ===============================================
 sudo apt install squid -y
-mkdir /var/lib/ssnvpn-pro/
-rm -f /usr/bin/menu-ssh
-wget -q -O /var/lib/ssnvpn-pro/ipvps.conf "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/ipvps.conf"
-wger -q -O /usr/bin/tendang "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/limit/tendang.sh"
-wget -q -O /usr/bin/autokill "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/limit/autokill.sh"
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu-ssh.sh"
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/menu/restart.sh"
-chmod +x /usr/bin/restart
-wget -q -O /usr/bin/bot "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Finaleuy/bot.sh"
-wget -q -O /root/chat "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/Finaleuy/chatid.sh"
-chmod 777 /root/chat
-chmod 777 /usr/bin/bot
-
-chmod +x /usr/bin/tendang
-chmod +x /usr/bin/autokill
-chmod +x /usr/bin/menu-ssh
 #setting IPtables
 iptables -I INPUT -p udp --dport 5300 -j ACCEPT
 iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
 netfilter-persistent save
 netfilter-persistent reload
 
-
-
 #delete directory
 rm -rf /root/nsdomain
 rm nsdomain
 
 #input nameserver manual to cloudflare
-
-
 read -rp "Masukkan Nameserver: " -e sub
 SUB_DOMAIN=${sub}
 NS_DOMAIN=${SUB_DOMAIN}
@@ -60,16 +40,13 @@ apt install -y dos2unix debconf-utils
 service cron reload
 service cron restart
 
-
-
-
 #konfigurasi slowdns
 rm -rf /etc/slowdns
 mkdir -m 777 /etc/slowdns
-wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.key"
-wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.pub"
-wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-server"
-wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-client"
+wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/NevermoreSSH/hopp/main/slowdns/server.key"
+wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/NevermoreSSH/hopp/main/slowdns/server.pub"
+wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/NevermoreSSH/hopp/main/slowdns/sldns-server"
+wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/NevermoreSSH/hopp/main/slowdns/sldns-client"
 cd
 chmod +x /etc/slowdns/server.key
 chmod +x /etc/slowdns/server.pub
