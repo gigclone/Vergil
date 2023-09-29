badd73b7e7e1bfbbfa28077c34fb419d1a9e3b26
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-29 12:15:30 +0800

    Update slow.sh

--- slow.sh
+++ slow.sh
@@ -3,7 +3,7 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Sabar sayang Lagi Proses"
-wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/autoscript-ssh-slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/addons/dns2.sh && chmod +x dns2.sh && ./dns2.sh
 echo -e " [INFO] Selesai Ganti Ns nya Sayang , Enter"
 sleep 2
 menu
